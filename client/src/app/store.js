866d57ffacabebe01210448da340229487d1862a
shramana263 <shramana652@gmail.com>
2025-03-13 02:58:37 +0530

    add authentication API with user registration and login functionality

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit"; 
+import authReducer from "../features/authSlice";
 
 export const appStore= configureStore({
     reducer:{
         //add reducers here
+        auth:authReducer
     }
 })
